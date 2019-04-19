de3646444d753f9b5c563c438a8767a1805697e0
raptorf1 <raptor_f1@hotmail.com>
2019-04-19 22:53:54 +0200

    Adds emoijis to display the result of each game visualy, prints the player's choise on the screen.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ PlayScissors = () => {
     var computer = choises[Math.floor(Math.random() * 30)];
     let showResult = document.getElementById("computerSelect");
     showResult.innerHTML = computer;
+    document.getElementById("playerChoise").innerHTML = "Scissors";
     let computerSelection = document.getElementById("computerSelect").innerHTML;
     if (computerSelection == "Paper") { document.getElementById("winner").innerHTML = "Congrats! You win.";}
         else if (computerSelection == "Rock") { document.getElementById("winner").innerHTML = "Too bad! Computer wins.";}
         else if (computerSelection == "Scissors") { document.getElementById("winner").innerHTML = "No winners! It's a tie.";}
+    Emoji()
 }
 
 PlayRock = () => {
@@ -14,10 +16,12 @@ PlayRock = () => {
     var computer = choises[Math.floor(Math.random() * 30)];
     let showResult = document.getElementById("computerSelect");
     showResult.innerHTML = computer;
+    document.getElementById("playerChoise").innerHTML = "Rock";
     let computerSelection = document.getElementById("computerSelect").innerHTML;
     if (computerSelection == "Paper") { document.getElementById("winner").innerHTML = "Too bad! Computer wins.";}
         else if (computerSelection == "Rock") { document.getElementById("winner").innerHTML = "No winners! It's a tie.";}
         else if (computerSelection == "Scissors") { document.getElementById("winner").innerHTML = "Congrats! You win.";}
+    Emoji()
 }
 
 PlayPaper = () => {
@@ -25,8 +29,26 @@ PlayPaper = () => {
     var computer = choises[Math.floor(Math.random() * 30)];
     let showResult = document.getElementById("computerSelect");
     showResult.innerHTML = computer;
+    document.getElementById("playerChoise").innerHTML = "Paper";
     let computerSelection = document.getElementById("computerSelect").innerHTML;
     if (computerSelection == "Paper") { document.getElementById("winner").innerHTML = "No winners! It's a tie.";}
         else if (computerSelection == "Rock") { document.getElementById("winner").innerHTML = "Congrats! You win.";}
         else if (computerSelection == "Scissors") { document.getElementById("winner").innerHTML = "Too bad! Computer wins.";}
+    Emoji()
+}
+
+Emoji = () => {
+    const winner = document.getElementById("winner").innerHTML;
+    if (winner == "No winners! It's a tie.") {
+        document.getElementById("tie").style.display = "block";
+        document.getElementById("playerWins").style.display = "none";
+        document.getElementById("playerLooses").style.display = "none";}
+        else if (winner == "Congrats! You win.") {
+            document.getElementById("playerWins").style.display = "block";
+            document.getElementById("tie").style.display = "none";
+            document.getElementById("playerLooses").style.display = "none";}
+        else if (winner == "Too bad! Computer wins.") {
+            document.getElementById("playerLooses").style.display = "block";
+            document.getElementById("tie").style.display = "none";
+            document.getElementById("playerWins").style.display = "none";}
 }
